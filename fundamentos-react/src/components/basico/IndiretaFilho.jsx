342c8313bf9f4138eb36abfeaebee06d466e1a51
Camyff <camily.franca01@etec.sp.gov.br>
2021-04-02 14:41:48 +0000

    Aula 02 - Fundamentos02
    Signed-off-by: Camyff <camily.franca01@etec.sp.gov.br>

---
+++ fundamentos-react/src/components/basico/IndiretaFilho.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export default props => {
+
+    const [nome, setNome] = useState('Bartholomew Jojo Simpson')
+
+    return(
+        <div>
+            <button onClick={() => { props.quandoClicar(nome) }} > Fornecer informações do aluno </button>
+        </div>
+    )
+}
